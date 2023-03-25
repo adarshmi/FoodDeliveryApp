46706b3a81b845a78d9364c5604b29d57b672026
adarshmi <adarshmi1234@gmail.com>
2023-03-25 17:51:25 +0530

    logout button working

--- restaurantapp/src/components/Header.jsx
+++ restaurantapp/src/components/Header.jsx
@@ -33,6 +33,16 @@ function Header() {
       setIsMenu(!isMenu);
     }
   };
+  const logout = () =>{
+    setIsMenu(false)
+    localStorage.clear()
+
+    dispatch({
+      type :actionType.SET_USER,
+      user: null,
+    });
+  }
+
   return (
     <header className="fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16">
       {/* Desktop & tablet */}
@@ -90,7 +100,12 @@ function Header() {
                     </p>
                   </Link>
                 )}
-                <p className="px-4 py-2 flex items-center cursor-pointer gap-3 hover:bg-slate-100 translate-all duration-100 ease-in-out text-textColor text-base">
+                <p className="px-4 py-2 flex items-center cursor-pointer gap-3
+                 hover:bg-slate-100 translate-all duration-100 
+                 ease-in-out text-textColor text-base"
+                 onClick={logout}
+                 
+                 >
                   Logout <MdLogout />
                 </p>
               </motion.div>
@@ -100,10 +115,18 @@ function Header() {
       </div>
       {/* mobile */}
       <div className="flex items-center justify-between md:hidden w-full h-full ">
-        <Link to={"/"} className="flex items-center gap-2">
+      
+        <div className="relative flex items-center justify-center ">
+            <MdShoppingBasket className="text-textColor text-2xl  cursor-pointer" />
+            <div className="absolute -top-2 -right-2 w-5 h-5  rounded-full bg-cartNumBg flex items-center justify-center">
+              <p className="text-xs text-white font-semibold">2</p>
+            </div>
+          </div>
+          <Link to={"/"} className="flex items-center gap-2">
           <img src={Logo} className="w-8 object-cover" alt="logo" />
           <p className="text-headingColor text-xl font-bold">city</p>
         </Link>
+          
         <div className="relative">
           <motion.img
             whileTap={{ scale: 0.6 }}
@@ -142,7 +165,13 @@ function Header() {
                 </li>
               </ul>
 
-              <p className="px-4 py-2 flex justify-center  items-center cursor-pointer gap-3 hover:bg-slate-300 bg-gray-200 translate-all duration-100 ease-in-out text-textColor text-base">
+              <p className="m-2 p-2  rounded-md shadow-md
+                flex justify-center  items-center 
+                cursor-pointer gap-3 hover:bg-gray-300
+                 bg-gray-200 translate-all duration-100 
+                 ease-in-out text-textColor text-base"
+                 onClick={logout}
+                 >
                 Logout <MdLogout />
               </p>
             </motion.div>
