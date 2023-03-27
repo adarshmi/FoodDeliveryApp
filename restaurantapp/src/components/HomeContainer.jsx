ff461854bd33bb133dcf4a4c1cc23ba450d25fd2
adarshmi <adarshmi1234@gmail.com>
2023-03-27 23:41:44 +0530

    new container added

--- restaurantapp/src/components/HomeContainer.jsx
+++ restaurantapp/src/components/HomeContainer.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React from "react";
 import Delivery from "../img/delivery.png";
 import HeroBG from "../img/heroBg.png";
 
+import { heroData } from "../utils/data";
+
 function HomeContainer() {
   return (
-    <section className=" grid grid-cols-1 md:grid-cols-2 gap-2 w-full" id="home" >
+    <section
+      className=" grid grid-cols-1 md:grid-cols-2 gap-2 w-full"
+      id="home"
+    >
       <div
         className="py-2 flex-1 flex flex-col  
       items-start justify-center gap-6"
@@ -28,24 +33,65 @@ function HomeContainer() {
           </div>
         </div>
         <p className="text-[2.5rem] lg:text-[4.5rem] font-bold tracking-wide text-headingColor">
-          The Fastest Delivery in <span className="text-orange-600 text-[3rem] lg:text-[5rem]">Your city</span>
+          The Fastest Delivery in{" "}
+          <span className="text-orange-600 text-[3rem] lg:text-[5rem]">
+            Your city
+          </span>
         </p>
         <p className="text-base text-textColor text-center md:text-left md:w-[80%]">
-        Magna consectetur fugiat laboris Lorem aliquip ea cupidatat ipsum sit reprehenderit aliquip. Eu minim ad minim consectetur. Adipisicing irure dolore ex duis enim labore qui reprehenderit deserunt anim dolore duis ullamco aliquip. Deserunt aute dolor ea id officia aliqua excepteur. Fugiat elit non deserunt voluptate consequat dolor do Lorem voluptate Lorem. Tempor incididunt irure aliqua culpa cillum sint et ipsum nostrud.
+          Magna consectetur fugiat laboris Lorem aliquip ea cupidatat ipsum sit
+          reprehenderit aliquip. Eu minim ad minim consectetur. Adipisicing
+          irure dolore ex duis enim labore qui reprehenderit deserunt anim
+          dolore duis ullamco aliquip. Deserunt aute dolor ea id officia aliqua
+          excepteur. Fugiat elit non deserunt voluptate consequat dolor do Lorem
+          voluptate Lorem. Tempor incididunt irure aliqua culpa cillum sint et
+          ipsum nostrud.
         </p>
 
-        <button type="button" className="bg-gradient-to-br from-orange-400 to-orange-500 w-full md:w-auto px-4 py-2 rounded-lg hover:shadow-lg transition-all ease-in-out duration-100 "> Order Now </button>
-
-
-
+        <button
+          type="button"
+          className="bg-gradient-to-br from-orange-400 to-orange-500 w-full md:w-auto px-4 py-2 rounded-lg hover:shadow-lg transition-all ease-in-out duration-100 "
+        >
+          {" "}
+          Order Now{" "}
+        </button>
       </div>
-      <div className="py-2 flex-1 flex w-full items-center">
-       
-          <img src={HeroBG} className="ml-auto  h-650" alt="hero-bg"/>
-        
+      <div className="py-2 flex-1 flex  items-center relative">
+        <img
+          src={HeroBG}
+          className="ml-auto h-420 w-full lg:w-auto lg:h-650"
+          alt="hero-bg"
+        />
+
+        <div className="w-full h-full absolute top-0 left-0 flex items-center justify-center lg:px-32  py-4 gap-4 flex-wrap">
+          {heroData &&
+            heroData.map((n) => (
+              <div
+                key={n.id}
+                className="  lg:w-190  p-4 bg-cardOverlay backdrop-blur-md rounded-3xl flex flex-col items-center justify-center drop-shadow-lg"
+              >
+                <img
+                  src={n.imageSrc}
+                  className="w-20 lg:w-40 -mt-10 lg:-mt-20 "
+                  alt="I1"
+                />
+                <p className="text-base lg:text-xl font-semibold text-textColor mt-2 lg:mt-4">
+                  {n.name}
+                </p>
+
+                <p className="text-[12px] lg:text-sm text-lighttextGray font-semibold my-1 lg:my-3">
+                  {n.decp}
+                </p>
+
+                <p className="text-sm font-semibold text-headingColor">
+                  <span className="text-xs text-red-600">$</span> {n.price}
+                </p>
+              </div>
+            ))}
+        </div>
       </div>
     </section>
-  )
+  );
 }
 
 export default HomeContainer;
---
+++ restaurantapp/src/utils/data.js
@@ -0,0 +1,12 @@
+import I1 from "../img/i1.png";
+import F1 from "../img/f1.png";
+import C3 from "../img/c1.png";
+import Fi1 from "../img/fi1.png";
+
+export const heroData = [
+  {id:1, name :'Icecream', decp: 'Chocolate & vanilla ',price :"5.25",imageSrc:I1},
+  {id:1, name :'Strawarries', decp: 'Fresh Strawarries ',price :"10.25",imageSrc:F1},
+  {id:1, name :'Chicken Kebab', decp: 'Mixed Kebab plate ',price :"8.25",imageSrc:C3},
+  {id:1, name :'Fish Kabab', decp: 'Mixed Fish Kabab ',price :"5.25",imageSrc:Fi1},
+  
+]
