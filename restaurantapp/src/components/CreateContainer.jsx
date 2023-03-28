312055413105e0dd9042fc6d699b65b433d11a6a
adarshmi <adarshmi1234@gmail.com>
2023-03-29 01:33:32 +0530

    new changes

--- restaurantapp/src/components/CreateContainer.jsx
+++ restaurantapp/src/components/CreateContainer.jsx
@@ -1,45 +1,69 @@
-import {useState ,React} from "react";
-import {motion} from "framer-motion";
-import {MdFastfood} from "react-icons/md"
+import { useState, React } from "react";
+import { motion } from "framer-motion";
+import { categories } from "../utils/data";
+import { MdFastfood } from "react-icons/md";
 
 function CreateContainer() {
-
-  
-  const [title, setTitle]= useState(" ");
-  const [calories, setCalories]= useState(" ");
-  const [price, setPrice]= useState(" ");
-  const [category, setCategory]= useState(null);
-  const [fields, setFields]= useState(false);
-  const [alartStatus, seAlartStatus]= useState("danger ");
-  const [msg, seMsg]= useState(null);
-
-  const [isLoading, setIsLoading]= useState(false);
-
-
+  const [title, setTitle] = useState("");
+  const [calories, setCalories] = useState("");
+  const [price, setPrice] = useState("");
+  const [category, setCategory] = useState(null);
+  const [imageAsset, setImageAsset] = useState(null);
+  const [fields, setFields] = useState(false);
+  const [alertStatus, setAlertStatus] = useState("danger");
+  const [msg, setMsg] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+ 
 
 
   return (
     <div className="w-full min-h-screen flex items-center justify-center ">
-     
       <div className=" w-[90%] md:w-[75%] border border-gray-300 rounded-lg p-4 flex flex-col items-center justfy-center  ">
-      {fields && (
-        <motion.p
-        initial = {{opacity:0}}
-        animate = {{opacity:1}}
-        exit = {{opacity:0}}
-        
-         className={`w-full p-2 rounded-lg text-center text-lg font-semibold ${
-          alartStatus === "danger" ? "bg-red-400 text-red-800":"bg-emerald-400 text-emerald-800"
-        } `}>
-        {msg}
-
-        </motion.p>
-      )}
-      <div className="w-full py-2 border-b border-gray-300 flex items-center gap-2 ">
-      <MdFastfood />
-
-      </div>
-       
+        {fields && (
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className={`w-full p-2 rounded-lg text-center text-lg font-semibold ${
+              alertStatus === "danger"
+                ? "bg-red-400 text-red-800"
+                : "bg-emerald-400 text-emerald-800"
+            } `}
+          >
+            {msg}
+          </motion.p>
+        )}
+        <div className="w-full py-2 border-b border-gray-300 flex items-center gap-2 ">
+          <MdFastfood className=" text-xl text-gray-700" />
+          <input
+            type="text"
+            required
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Give me a title..."
+            className="w-full h-full bg-transparent font-semibold outline-none border-none placeholder:text-gray-500 text-textColor "
+          />
+        </div>
+        <div className="w-full">
+        <select
+            onChange={(e) => setCategory(e.target.value)}
+            className="outline-none w-full text-base border-b-2 border-gray-200 p-2 rounded-md cursor-pointer"
+          >
+            <option value="other" className="bg-white">
+              Select Category
+            </option>
+            {categories &&
+              categories.map((item) => (
+                <option
+                  key={item.id}
+                  className="text-base border-0 outline-none capitalize bg-white text-headingColor"
+                  value={item.urlParamName}
+                >
+                  {item.name}
+                </option>
+              ))}
+          </select>
+        </div>
       </div>
     </div>
   );
--- restaurantapp/src/utils/data.js
+++ restaurantapp/src/utils/data.js
@@ -4,9 +4,70 @@ import C3 from "../img/c1.png";
 import Fi1 from "../img/fi1.png";
 
 export const heroData = [
-  {id:1, name :'Icecream', decp: 'Chocolate & vanilla ',price :"5.25",imageSrc:I1},
-  {id:1, name :'Strawarries', decp: 'Fresh Strawarries ',price :"10.25",imageSrc:F1},
-  {id:1, name :'Chicken Kebab', decp: 'Mixed Kebab plate ',price :"8.25",imageSrc:C3},
-  {id:1, name :'Fish Kabab', decp: 'Mixed Fish Kabab ',price :"5.25",imageSrc:Fi1},
-  
-]
+  {
+    id: 1,
+    name: "Icecream",
+    decp: "Chocolate & vanilla ",
+    price: "5.25",
+    imageSrc: I1,
+  },
+  {
+    id: 1,
+    name: "Strawarries",
+    decp: "Fresh Strawarries ",
+    price: "10.25",
+    imageSrc: F1,
+  },
+  {
+    id: 1,
+    name: "Chicken Kebab",
+    decp: "Mixed Kebab plate ",
+    price: "8.25",
+    imageSrc: C3,
+  },
+  {
+    id: 1,
+    name: "Fish Kabab",
+    decp: "Mixed Fish Kabab ",
+    price: "5.25",
+    imageSrc: Fi1,
+  },
+];
+export const categories = [
+  {
+    id: 1,
+    name: "Chicken",
+    urlParamName: "chicken",
+  },
+  {
+    id: 2,
+    name: "Curry",
+    urlParamName: "curry",
+  },
+  {
+    id: 3,
+    name: "Rice",
+    urlParamName: "rice",
+  },
+  {
+    id: 4,
+    name: "Fish",
+    urlParamName: "fish",
+  },
+  {
+    id: 5,
+    name: "Fruits",
+    urlParamName: "fruits",
+  },
+  {
+    id: 6,
+    name: "Icecreams",
+    urlParamName: "icecreams",
+  },
+
+  {
+    id: 7,
+    name: "Soft Drinks",
+    urlParamName: "drinks",
+  },
+];
