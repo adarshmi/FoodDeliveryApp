244c25484958a21601823c5f1fabef8ac7b55beb
adarshmi <adarshmi1234@gmail.com>
2023-03-30 23:48:46 +0530

    main contener update

--- restaurantapp/src/components/MainContainer.jsx
+++ restaurantapp/src/components/MainContainer.jsx
@@ -6,6 +6,15 @@ function MainContainer() {
   return (
     <div className=" w-full h-auto flex flex-col items-center justify-center ">
      <HomeContainer />
+     <section className="w-full ">
+     <div className="w-full flex items-center justify-between">
+     <p className="text-lg font-semibold capitalize text-headingColor relative">
+      Our fresh & healthy fruits 
+     </p>
+
+     </div>
+
+     </section>
     </div>
   );
 }
